56597551a9ed903e332bd738a59d1a4a950f25dc
laihaoqiang <laihaoqiang34@gmail.com>
2024-03-20 17:10:26 +1000

    header complete

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+	function TopNav() {
+		return (
+			<div className="flex gap-20 justify-center my-5 text-white text-xl">
+				<div className="flex items-center gap-4"><img src="src/assets/icons/searchicon.svg" alt="" className="w-7"/>What are you looking for?</div>
+				<div className="flex items-center gap-4"><img src="src/assets/icons/phoneicon.svg" alt="" className="w-7"/>+1 250 585 7880</div>
+				<div className="flex items-center gap-4"><img src="src/assets/icons/emailicon.svg" alt="" className="w-7"/>info@allenmarine.ca</div>
+				<div className="flex items-center gap-4"><img src="src/assets/icons/regionicon.svg" alt="" className="w-7"/>2229 McCullough Rd, Nanaimo, BC, V9S 4M7</div>
+			</div>
+		)
+	}
+	return (
+		<div className="mx-40 bg-green-400">
+			<TopNav />
+			<hr/>
+			<div className="flex justify-between items-center my-10 text-xl font-bold">
+				<img src="src/assets/images/logo.png" alt="" />
+				<div className="flex gap-10 text-white">
+					<Link to="#">SHOWROOM</Link>
+					<Link to="#">SERVICES</Link>
+					<Link to="#">FINANCING</Link>
+					<Link to="#">ABOUT US</Link>
+					<Link to="#">CONTACT US!</Link>
+				</div>
+				<button className="bg-[#BF9550] text-white p-3 rounded-full">APPLY FOR FINANCING</button>
+			</div>
+		</div>
+	)
+}
+
+export default Header;
